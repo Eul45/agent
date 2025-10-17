3318d06cd0b814189803792c749dc9b63520ebfa
Eyuel <nenjose22@gmail.com>
2025-10-17 23:49:15 +0300

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,1 +1,11 @@
-
+import { Ionicons } from '@expo/vector-icons';
+import { Content, GoogleGenerativeAI } from '@google/generative-ai';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+// ADD THIS at the top of your file with other imports
+import * as Clipboard from 'expo-clipboard';
+import * as Device from 'expo-device';
+import * as DocumentPicker from 'expo-document-picker';
+import * as FileSystem from 'expo-file-system';
+import * as ImagePicker from 'expo-image-picker';
+import * as MediaLibrary from 'expo-media-library';
+import { ExpoSpeechRecognitionModule } from "expo-speech-recognition";
